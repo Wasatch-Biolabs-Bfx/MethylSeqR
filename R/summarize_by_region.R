ca972f581aaa1a2a56391ef4b1fb6783d497dada
Hailey Johnson <123395871+haileye4@users.noreply.github.com>
2024-07-16 11:07:58 -0600

    Update summarize_by_region.R

--- R/summarize_by_region.R
+++ R/summarize_by_region.R
@@ -4,30 +4,18 @@ summarize_by_region <- function(modseq_dat, annotation)
   if (ncol(annotation) < 3 || ncol(annotation) > 4) {
     stop("Annotation must be in correct format. ex. chrom, start, end, region_name OR chrom, start, end")
   }
-
+  
   # Create regional dataframe
-  print("stuck 1")
-  modseq_dat <- right_join(modseq_dat, annotation, copy = TRUE,
-                           by = join_by(chrom, between(ref_position, start, end))) |>
-    summarize(mean_cov = mean(cov, na.rm = TRUE))
+  modseq_dat <- right_join(modseq_dat, annotation, 
+                           by = join_by(chrom, between(ref_position, start, end)))
   
-  print("stuck 2")
-  
-  if ("mh_frac" %in% colnames(modseq_dat)) {
-    modseq_dat = modseq_dat |>
-      summarize(mean_mh_frac = sum(cov * mh_frac) / sum(cov))
-  }
-  
-  print("stuck 3")
-  
-  if ("m_frac" %in% colnames(modseq_dat)) {
-    modseq_dat = modseq_dat |>
-      summarize(mean_m_frac = sum(cov * m_frac) / sum(cov))
-  }
-  
-  print("stuck 4")
-  if ("h_frac" %in% colnames(modseq_dat)) {
-    modseq_dat = modseq_dat |>
-      summarize(mean_h_frac = sum(cov * h_frac) / sum(cov))
-  }
+    # Calculate the requested scores- summarize scores based on available columns
+  modseq_dat <- modseq_dat %>%
+    summarize( .by = c(sample_name, region_name),
+      mean_mh_frac = if ("mh_frac" %in% colnames(modseq_dat)) sum(cov * mh_frac) / sum(cov) else NA_real_,
+      mean_m_frac = if ("m_frac" %in% colnames(modseq_dat)) sum(cov * m_frac) / sum(cov) else NA_real_,
+      mean_h_frac = if ("h_frac" %in% colnames(modseq_dat)) sum(cov * h_frac) / sum(cov) else NA_real_,
+      mean_cov = mean(cov, na.rm = TRUE)
+  ) |>
+  select_if(~ !all(is.na(.)))
 }
