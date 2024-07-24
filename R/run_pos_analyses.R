acfca3c4d7112f9da1edce3bbe0d0fbc313c0a4e
haileye4 <hailey.e.johnson3@gmail.com>
2024-07-24 14:24:38 -0600

    changed to regional

---
+++ R/run_pos_analyses.R
@@ -0,0 +1,15 @@
+run_pos_analyses <- function(modseq_dat,
+                             annot_file,
+                             cases,
+                             controls) {
+  # wrapper function
+  modseq_dat |>
+    summarize_by_pos() |>
+    aggregate_regions(
+      annot_file = annot_file) |>
+    calc_mod_diff(
+      cases = cases,
+      controls = controls,
+      calc_type = "fast_fisher") |>
+    compute()
+}
