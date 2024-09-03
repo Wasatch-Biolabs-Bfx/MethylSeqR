3b05bf7cefaba3c36ea3fdd1f78d8fcecea97e1a
haileye4 <hailey.e.johnson3@gmail.com>
2024-09-03 14:59:07 -0600

    recompile

---
+++ R/calc_windows.R
@@ -0,0 +1,95 @@
+## Sliding Window
+
+# Dependencies
+library(tidyr)
+library(dplyr)
+library(dbplyr)
+library(duckdb)
+library(duckplyr)
+library(progress)
+
+# Function
+calc_windows <- function(db_folder,
+                         call_type = "positions",
+                         window_size = 1000,
+                         step_size = 10,
+                         overwrite = TRUE) 
+{
+  # If a character file name is provided, then make ch3 class obj
+  if (inherits(ch3_db, "character")) {
+    ch3_db <- list(db_file = ch3_db)
+    class(ch3_db) <- "ch3_db"
+    db_con <- dbConnect(duckdb(ch3_db$db_file), read_only = FALSE)
+    ch3_db$tables <- dbListTables(db_con)
+  } else if (inherits(ch3_db, "character")) {
+    db_con <- dbConnect(duckdb(ch3_db$db_file), read_only = FALSE)
+  } else {
+    stop("Invalid ch3_db class. Must be character or ch3_db.")
+  }
+  
+  # Check if needed table exists
+  stopifnot("X Table does not exist. You can create it by..." =
+              "X" %in% ch3_db$tables)
+  
+  if (dbExistsTable(db_con, "windows") & overwrite)
+    dbRemoveTable(db_con, "windows")
+  
+  if (dbExistsTable(db_con, "temp_table"))
+    dbRemoveTable(db_con, "temp_table")
+  
+  # Create Progress Bar
+  pb <- progress_bar$new(
+    format = "[:bar] :percent [Elapsed time: :elapsed]",
+    total = length(offsets) + 1,
+    complete = "=",   
+    incomplete = "-", 
+    current = ">",    
+    clear = FALSE,    
+    width = 100)   
+  
+  # Calc windows in each frame
+  offsets <- seq(1, window_size - 1, by = step_size)
+  
+  # Tick progress bar to make it show up (first loop can be long)
+  pb$tick()
+  
+  # Conduct analysis. 
+  # Creates tiled windows and then loops to create sliding window
+  for (offset in offsets) {
+    tbl(
+      db_con, call_type) |>
+      mutate(
+        start = ref_position - ((ref_position - offset) %% window_size)) |>
+      filter(
+        start > 0) |>
+      summarize(
+        .by = c(sample_name, chrom, start),
+        total_calls = sum(cov),
+        across(ends_with("_counts"), ~sum(.x, na.rm = TRUE)),
+        across(ends_with("_frac"), ~sum(.x * cov, na.rm = TRUE) / sum(cov))) |>
+      mutate(
+        end = start + window_size - 1) |>
+      compute(name = "temp_table", temporary = TRUE)
+    
+    # Create or append table
+    dbExecute(db_con, 
+              "CREATE TABLE IF NOT EXISTS windows AS 
+              SELECT * FROM temp_table WHERE 1=0")
+    
+    dbExecute(db_con, 
+              "INSERT INTO windows SELECT * FROM temp_table")
+    
+    dbRemoveTable(db_con, "temp_table")
+    
+    # Advance progress bar
+    pb$tick()
+  }
+  
+  # Close progress bar
+  pb$terminate()
+  
+  # Finish Up
+  ch3_db$tables <- dbListTables(db_con) # Update table list
+  dbDisconnect(db_con, shutdown = TRUE)
+  return(ch3_db)
+}
---
+++ R/make_pos_db.R
@@ -0,0 +1,87 @@
+#' Summarize methylation data by reference positions
+#'
+#' @param modseq_dat Methylation data in a duckdb object as opened by open_dat().
+#' @param score A methylation score type. Default is mh, can also include m, h, or a vector of score types wanted.
+#' @return A duckdb object of methylation scores summarized by each position.
+#' @examples
+#' make_pos_db(data)
+#' make_pos_db(data, score = "m")
+#' make_pos_db(data, score = c("m", "mh", "h")
+#' @import arrow tidyr dplyr dbplyr duckdb duckplyr
+#' @export
+make_pos_db <- function(ch3_files,
+                        ch3_db,
+                        min_call_prob = 0.9,
+                        min_length = 100,
+                        min_base_qual = 10)
+{
+  # Setup files and db
+  if (!grepl(".ch3.db$", ch3_db))
+    ch3_db <- paste0(ch3_db, ".ch3.db")
+
+  ch3_db <- list(db_file = ch3_db)
+  class(ch3_db) <- "ch3_db"
+  db_con <- dbConnect(duckdb(ch3_db$db_file), read_only = FALSE)
+  ch3_db$tables <- dbListTables(db_con)
+
+  ch3_files <- list.files(ch3_files, pattern = "\\.ch3$", full.names = TRUE)
+
+  # Check if the table already exists and delete it if it does
+  if (dbExistsTable(db_con, "positions"))
+    dbRemoveTable(db_con, "positions")
+
+  # Loop through files to add to db
+  # Create Progress Bar
+  pb <- progress_bar$new(
+    format = "[:bar] :percent [Elapsed time: :elapsed]",
+    total = length(ch3_files) + 1,
+    complete = "=",
+    incomplete = "-",
+    current = ">",
+    clear = FALSE,
+    width = 100)
+
+  pb$tick()
+
+  for (ch3_file in ch3_files) {
+    open_dataset(ch3_file) |>
+      select(sample_name, chrom, ref_position, call_prob,
+             read_length, base_qual, call_code) |>
+      filter(
+        call_prob >= min_call_prob,
+        read_length >= min_length,
+        base_qual >= min_base_qual) |>
+      summarize(
+        .by = c(sample_name, chrom, ref_position),
+        cov = n(),
+        c_counts = sum(as.integer(call_code == "-"),
+                       na.rm = TRUE),
+        m_counts = sum(as.integer(call_code == "m"),
+                       na.rm = TRUE),
+        h_counts = sum(as.integer(call_code == "h"),
+                       na.rm = TRUE),
+        mh_counts = sum(as.integer(call_code %in% c("m", "h")),
+                        na.rm = TRUE)) |>
+      mutate(
+        m_frac = m_counts / cov,
+        h_frac = h_counts / cov,
+        mh_frac = mh_counts / cov) |>
+      arrange(
+        sample_name, chrom, ref_position) |>
+      collect() |>
+      dbWriteTable(
+        conn = db_con,
+        name = "positions",
+        append = TRUE)
+
+    pb$tick()
+  }
+
+  # Close progress bar
+  pb$terminate()
+
+  # Finish Up
+  ch3_db$tables <- dbListTables(db_con) # Update table list
+  dbDisconnect(db_con, shutdown = TRUE)
+  return(ch3_db)
+}
