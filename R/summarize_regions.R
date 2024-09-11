b25eaa3abffe84ad2246151e306401b68ba85b2d
haileye4 <hailey.e.johnson3@gmail.com>
2024-09-11 15:26:47 -0600

    new summarize regions function

--- R/summarize_regions.R
+++ R/summarize_regions.R
@@ -1,79 +1,108 @@
-#' Aggregate positional methylation data by regions provided with an annotation file.
-#'
-#' @param modseq_dat A duckdb object of methylation data already processed with summarize_by_pos().
-#'
-#' @param annot_file Path to an annotation file of regions to aggregate data into- TSV format.
-#' This file is required to have three columns- chrom, start, end. Optional fourth column of region_name can be included.
-#'
-#' @return A duckdb object of all regional methylation data to be processed.
-#'
-#' @examples
-#' regional_data = aggregate_regions(data, "CpG_islands.tsv")
-#'
-#' @import dplyr readr
-#'
-#' @export
-aggregate_regions <- function(modseq_dat,
-                              annot_file,
+library(readr)
+library(tidyr)
+library(dplyr)
+library(dbplyr)
+library(duckdb)
+library(duckplyr)
+
+# Function
+summarize_regions <- function(ch3_db,
+                              region_bed,
                               join_type = "inner")
 {
   # Read annotation
   annotation <-
-    read_csv(annot_file,
+    read_csv(region_bed,
              col_names = c("chrom", "start", "end", "region_name"),
              show_col_types = FALSE)
-
+  
   # make sure column names did not get included and mess up code...
   if (annotation[1,1] %in% c("chr", "Chr", "chrom", "Chrom")) {
     annotation = annotation[-1, ]
   }
-
   if (ncol(annotation) < 3 || ncol(annotation) > 4) {
     stop("Invalid annotation format. File must have 3 or 4 columns:
          chr, start, end, region_name (optional) annotation.")
   }
-
   if (!join_type %in% c("inner", "right", "left", "full")) {
     stop("Invalid join type")
   }
-
   if (ncol(annotation == 3)) {
     annotation <-
       annotation |>
       mutate(
         region_name = paste(chrom, start, end, sep = "_"))
   }
-
+  
   annotation <-
     annotation |>
     reframe(
       .by = c(region_name, chrom),
       ref_position = start:end)
-
+  
+  # Open the database connection
+  if (inherits(ch3_db, "character")) {
+    ch3_db <- list(db_file = ch3_db)
+    class(ch3_db) <- "ch3_db"
+    db_con <- dbConnect(duckdb(ch3_db$db_file), read_only = FALSE)
+    ch3_db$tables <- dbListTables(db_con)
+  } else if (inherits(ch3_db, "ch3_db")) {
+    db_con <- dbConnect(duckdb(ch3_db$db_file), read_only = FALSE)
+  } else {
+    stop("Invalid ch3_db class. Must be character or ch3_db.")
+  }
+  
   # Create regional data frame- offer a left, right or inner join
-
+  
   # left join, if they wanna keep reads that are outside of the annotation table
-
+  
   # right join, if they want to keep reads that are all in annotation table and
   # even those regions not included in data frame
-
+  
   # inner join- regions in both annotation and data frame...
   my_join <- switch(join_type,
                     "inner" = inner_join,
                     "right" = right_join,
                     "left" = left_join,
                     "full" = full_join)
-
-  modseq_dat |>
-    my_join( # make join
-      annotation,
-      by = join_by(chrom, ref_position),
-      copy = TRUE) |>
+  
+  # Initialize progress bar (for the number of regions to be processed)
+  # pb <- progress_bar$new(
+  #   format = "[:bar] :percent [Elapsed time: :elapsed]",
+  #   total = nrow(annotation),  # Progress based on number of annotation regions
+  #   complete = "=",   
+  #   incomplete = "-", 
+  #   current = ">",    
+  #   clear = FALSE,    
+  #   width = 100
+  # )
+  
+  # make a loop through chr
+  # "diff_meth" 
+  
+  # Begin summarizing by region
+  # Perform the join and aggregation
+  regional_data <- tbl(db_con, "positions") |>
+    my_join(annotation, 
+            by = join_by(chrom, 
+                         ref_position), 
+            copy = TRUE) |>
     summarize(
       .by = c(sample_name, region_name),
       cov = sum(cov),
       across(ends_with("_counts"), sum),
-      across(ends_with("_frac"), ~ sum(.x * cov) / sum(cov))) %>%
-    collect()
-
+      across(ends_with("_frac"), ~ sum(.x * cov) / sum(cov))) |>
+    compute(name = "temp_table", temporary = TRUE)
+  
+  # Update progress bar for each iteration (in this case, for each region)
+  # pb$tick()
+  
+  # Write the aggregated data to the database (if needed)
+  dbWriteTable(conn = db_con, name = "regions", value = regional_data, overwrite = TRUE)
+  
+  # Finish up: update table list and close the connection
+  ch3_db$tables <- dbListTables(db_con)
+  dbDisconnect(db_con, shutdown = TRUE)
+  
+  return(ch3_db)
 }
