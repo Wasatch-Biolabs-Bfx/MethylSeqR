159054db5eea526b4880857f73ca25b4dd07f8b6
haileye4 <hailey.e.johnson3@gmail.com>
2024-10-02 19:52:26 -0600

    Summarize_regions() workinggit add .

--- R/helper_print.R
+++ R/helper_print.R
@@ -1,29 +1,29 @@
-helper_print <- function(ch3_db, tables = "all")
-{
+helper_print <- function(ch3_db, tables = "all") {
   # Open the database connection
   db_con <- helper_connectDB(ch3_db)
   
-   tryCatch(
+  tryCatch(
     {
       # List all tables in the database
       all_tables <- dbListTables(db_con)
-      message(paste0("All tables currently in database: "))
+      cat("All tables currently in database:\n")
       
+      # Print each table name
       for (tbl_name in all_tables) {
-        print(tbl_name)
+        cat(tbl_name, "\n")
       }
       
       if (tables == "all") {
-        tables = all_tables
+        tables <- all_tables
       }
       
       for (tb_name in tables) {
         if (tb_name %in% all_tables) {
           # Print the table name
-          message(paste0("Table: ", tb_name))
+          cat("Table: ", tb_name, "\n")
           
           # Fetch the first few rows and print them
-          print(tbl(db_con, tb_name) %>% head())
+          print(head(tbl(db_con, tb_name)))
           
         } else {
           # Print a message if the table does not exist
@@ -31,13 +31,12 @@ helper_print <- function(ch3_db, tables = "all")
         }
       }
     }, 
-    error = function(e) 
-    {
+    error = function(e) {
       # Print custom error message
       message("An error occurred: ", e$message)
     }, 
-    finally = 
-      {
-        helper_closeDB(ch3_db, db_con)
-      })
+    finally = {
+      helper_closeDB(ch3_db, db_con)
+    }
+  )
 }
--- R/summarize_regions.R
+++ R/summarize_regions.R
@@ -13,7 +13,9 @@ summarize_regions <- function(ch3_db,
   # Read annotation
   annotation <-
     read_csv(region_bed,
-             col_names = c("chrom", "start", "end", "region_name"),
+             col_names = c("chrom", "start", 
+                           "end", 
+                           "region_name"),
              show_col_types = FALSE)
   
   # make sure column names did not get included and mess up code...
@@ -39,85 +41,103 @@ summarize_regions <- function(ch3_db,
     reframe(
       .by = c(region_name, chrom),
       ref_position = start:end)
-  
-  print(head(annotation))
-  
-  # Open the database connection
-  db_con <- helper_connectDB(ch3_db)
-  
-  # Create regional data frame- offer a left, right or inner join
-  
-  # left join- keep reads that are outside of the annotation table
-  # right join- keep reads in annotation table + regions not included in data frame
-  # inner join- regions in both annotation and data frame...
-  if (!join_type %in% c("inner", "right", "left", "full")) {
-    stop("Invalid join type")
-  }
-  
-  my_join <- switch(join_type,
-                    "inner" = inner_join,
-                    "right" = right_join,
-                    "left" = left_join,
-                    "full" = full_join)
-  
-  if (dbExistsTable(db_con, "regions"))
-    dbRemoveTable(db_con, "regions")
-  
-  # Create regions table by unique chroms
-  chroms <-
-    annotation |>
-    select(chrom) |>
-    distinct() |>
-    filter(nchar(chrom) < 6) |>
-    pull()
-  
-  # Create Progress Bar
-  pb <- progress_bar$new(
-    format = "[:bar] :percent [Elapsed time: :elapsed]",
-    total = length(chroms) + 1,
-    complete = "=",
-    incomplete = "-",
-    current = ">",
-    clear = FALSE,
-    width = 100)
-  
-  pb$tick()
-  
-  for (chr in chroms) {
-    # Begin summarizing by region- perform the join and aggregation
-    tbl(db_con, "positions") |>
-      filter(chrom == chr) |>
-      my_join(annotation, 
-              by = join_by(chrom, 
-                           ref_position), 
-              copy = TRUE) |>
-      summarize(
-        .by = c(sample_name, region_name),
-        cov = sum(cov),
-        across(ends_with("_counts"), sum),
-        across(ends_with("_frac"), ~ sum(.x * cov) / sum(cov))) |>
-      compute(name = "temp_table", temporary = TRUE)
-    
-    # Create or append table
-    dbExecute(db_con, 
-              "CREATE TABLE IF NOT EXISTS regions AS 
+
+  tryCatch(
+    {
+      # Open the database connection
+      db_con <- helper_connectDB(ch3_db)
+      # Create regional data frame- offer a left, right or inner join
+      
+      # left join- keep reads that are outside of the annotation table
+      # right join- keep reads in annotation table + regions not included in data frame
+      # inner join- regions in both annotation and data frame...
+      if (!join_type %in% c("inner", "right", "left", "full")) {
+        stop("Invalid join type")
+      }
+      
+      my_join <- switch(join_type,
+                        "inner" = inner_join,
+                        "right" = right_join,
+                        "left" = left_join,
+                        "full" = full_join)
+      
+      if (dbExistsTable(db_con, "regions"))
+        dbRemoveTable(db_con, "regions")
+      
+      # Create regions table by unique chroms
+      chroms <-
+        annotation |>
+        select(chrom) |>
+        distinct() |>
+        filter(nchar(chrom) < 6) |>
+        pull()
+      
+      # Create Progress Bar
+      pb <- progress_bar$new(
+        format = "[:bar] :percent [Elapsed time: :elapsed]",
+        total = length(chroms) + 1,
+        complete = "=",
+        incomplete = "-",
+        current = ">",
+        clear = FALSE,
+        width = 100)
+      
+      pb$tick()
+      
+      db_tbl = tbl(db_con, "positions")
+      
+      for (chr in chroms) {
+        # Begin summarizing by region- perform the join and aggregation
+        db_tbl |>
+          filter(chrom == chr) |>
+          my_join(annotation, 
+                  by = join_by(chrom, 
+                               ref_position), 
+                  copy = TRUE) |>
+          summarize(
+            .by = c(sample_name, region_name),
+            cov = sum(cov),
+            across(ends_with("_counts"), sum),
+            across(ends_with("_frac"), ~ sum(.x * cov) / sum(cov))) |>
+          compute(name = "temp_table", temporary = TRUE)
+        
+        # Create or append table
+        dbExecute(db_con, 
+                  "CREATE TABLE IF NOT EXISTS regions AS 
               SELECT * FROM temp_table WHERE 1=0")
-    
-    dbExecute(db_con, 
-              "INSERT INTO regions SELECT * FROM temp_table")
-    
-    dbRemoveTable(db_con, "temp_table")
-    
-    pb$tick()
-  }
-  
-  # Close progress bar
-  pb$terminate()
-  
-  # Finish up: purge extra tables & update table list and close the connection
-  helper_purgeTables(db_con, keep_tables = c("positions", "regions"))
-  tbl(db_con, "regions")
-  helper_closeDB(ch3_db, db_con)
-  
+        
+        dbExecute(db_con, 
+                  "INSERT INTO regions SELECT * FROM temp_table")
+        
+        dbRemoveTable(db_con, "temp_table")
+        
+        pb$tick()
+      }
+      
+      # Close progress bar
+      pb$terminate()
+    }, error = function(e)
+    {
+      # Print custom error message
+      message("An error occurred: ", e$message)
+      dbRemoveTable(db_con, "regions")
+    }, 
+    finally = 
+      {
+        # Finish up: purge extra tables & update table list and close the connection
+        keep_tables = c("positions", "regions", "windows", "meth_diff")
+        # List all tables in the database
+        all_tables <- dbListTables(db_con)
+        # Remove tables that are not in the 'keep_tables' list
+        for (table in all_tables) {
+          if (!(table %in% keep_tables)) {
+            dbRemoveTable(db_con, table)
+          }
+        }
+        
+        helper_closeDB(ch3_db, db_con)
+      }
+  )
+
   return(ch3_db)
 }
