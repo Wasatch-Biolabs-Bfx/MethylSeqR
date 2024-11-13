fd1161b00d03d6f14e4702fb97654bd1f8f55055
haileye4 <hailey.e.johnson3@gmail.com>
2024-11-13 10:32:31 -0700

    fixed print

--- R/helper_internal_print.R
+++ R/helper_internal_print.R
@@ -1,13 +1,10 @@
-#' Helper Function to Print Database Tables
+#' Internal Helper Function to Print Database Tables after creation
 #'
 #' This function connects to a DuckDB database and prints the names of all tables currently in the 
 #' database. It can also print the first few rows of specific tables.
 #'
 #' @param ch3_db A character string or an object of class `ch3_db` representing the DuckDB database to connect to.
-#' @param tables A character vector specifying the names of tables to print. Defaults to the last table given, which 
-#' will print all tables. If specific table names are provided, it will only print those (ex. "positions", "regions", "meth_diff").
-#' If tables ="all", the function will print out all table sin the database...
-#'
+#' 
 #' @details
 #' The function establishes a connection to the database, retrieves the list of tables, and prints their 
 #' names. If the specified table exists, it also prints the first few rows of that table. If a specified 
@@ -16,17 +13,9 @@
 #' @return None. This function is called for its side effects (printing information).
 #'
 #' @import DBI
-#' 
-#' @examples 
-#' # Specify the path to the database
-#'  ch3_db <- system.file("my_data.ch3.db", package = "MethylSeqR")
-#'  
-#'  # Print out tables in the database
-#'  print_db(ch3_db, tables = "all")
 #'
-#' @export
-
-print_db <- function(ch3_db, tables = "all") {
+#' @keywords internal
+print.ch3_db <- function(ch3_db) {
   # Open the database connection
   database <- .helper_connectDB(ch3_db)
   db_con <- database$db_con
@@ -47,24 +36,17 @@ print_db <- function(ch3_db, tables = "all") {
       }
       cat("\n")
       
-      if (tables == "all") {
-        tables <- all_tables
-      } else if (tables == "") {
-        tables <- database$last_table
-      }
       
-      for (tb_name in tables) {
-        if (tb_name %in% all_tables) {
-          # Print the table name
-          cat("Printing table: ", tb_name, "\n")
-          
-          # Fetch the first few rows and print them
-          print(head(tbl(db_con, tb_name)))
-          
-        } else {
-          # Print a message if the table does not exist
-          message(paste0("Table '", tb_name, "' does not exist in the database."))
-        }
+      if (database$last_table %in% all_tables) {
+        # Print the table name
+        cat("Printing table: ", tb_name, "\n")
+        
+        # Fetch the first few rows and print them
+        print(head(tbl(db_con, tb_name)))
+        
+      } else {
+        # Print a message if the table does not exist
+        message(paste0("Table '", tb_name, "' does not exist in the database."))
       }
     }, 
     error = function(e) {
