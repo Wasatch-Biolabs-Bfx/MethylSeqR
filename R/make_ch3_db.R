6c7afde5169e7d1cc3287e09b19954064f83b19e
haileye4 <hailey.e.johnson3@gmail.com>
2025-02-13 14:15:18 -0700

    exporting make_ch3_db

--- R/make_ch3_db.R
+++ R/make_ch3_db.R
@@ -1,3 +1,45 @@
+#' Create a CH3 Database from Parquet Files
+#'
+#' This function processes a collection of CH3 files stored in Parquet format and creates 
+#' a DuckDB database containing filtered and structured methylation call data.
+#'
+#' @param ch3_files A character vector of file or directory paths containing CH3 Parquet files. 
+#' If a directory is provided, all files within it will be processed.
+#' @param db_name A string representing the path where the CH3 database will be created. 
+#' The extension ".ch3.db" will be appended if not already present.
+#' @param chrom An optional character string specifying a chromosome to filter the data. 
+#' If NULL, all chromosomes will be included.
+#' @param min_read_length A numeric value specifying the minimum read length required 
+#' for inclusion in the database. Default is 50.
+#' @param min_call_prob A numeric value representing the minimum call probability threshold. 
+#' Only calls with a probability greater than or equal to this value will be included. Default is 0.9.
+#' @param min_base_qual A numeric value representing the minimum base quality threshold. 
+#' Only reads with quality scores at or above this value will be included. Default is 30.
+#' @param flag An optional numeric value specifying a flag-based filter for the data. 
+#' If NULL, no flag filtering is applied.
+#'
+#' @details
+#' This function reads CH3 files stored in Parquet format and imports them into a DuckDB database. 
+#' The data is filtered based on user-specified criteria, including chromosome, read length, 
+#' call probability, base quality, and flag values. If a table already exists in the database, 
+#' it will be dropped and recreated.
+#'
+#' @return A list of class `"ch3_db"`, containing:
+#' \item{db_file}{The path to the created DuckDB database file.}
+#' \item{tables}{A list of tables available in the database.}
+#'
+#' @import duckdb
+#' @import dbplyr
+#'
+#' @examples
+#' # Example usage
+#' ch3_files <- system.file("new_test_data", package = "MethylSeqR")
+#' db_name <- tempfile("example_ch3")
+#' 
+#' result <- make_ch3_db(ch3_files, db_name)
+#' print(result)
+#'
+#' @export
 make_ch3_db <- function(ch3_files, 
                         db_name,
                         chrom = NULL, 
