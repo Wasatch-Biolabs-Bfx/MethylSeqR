9e8a342cb057b5c517fa8d1479dfa82249e8953b
haileye4 <hailey.e.johnson3@gmail.com>
2025-02-24 14:54:02 -0700

    downsample function

--- R/sample_ch3.R
+++ R/sample_ch3.R
@@ -7,8 +7,7 @@
 #' @param out_path A string specifying the directory where the output CH3 file will be saved.
 #' @param coverage A numeric value specifying the desired coverage for the downsampled CH3 file. Defaults to 5.
 #' @param sample_name_suffix A string to append to the sample name in the output file. Defaults to an empty string.
-#' @param genome_size A numeric value representing the size of the genome in base pairs. Defaults to 3.2e9 (human genome size).
-#'
+#' 
 #' @details
 #' This function calculates the current coverage of the input CH3 file based on the 
 #' genome size and read lengths. If the current coverage is less than the desired coverage, 
@@ -41,16 +40,10 @@
 
 sample_ch3 <- function(ch3_file,
                        out_path,
-                       coverage = 5, 
-                       sample_name_suffix = "",
-                       genome_size = 3.2e9)
+                       coverage, 
+                       sample_name_suffix = "")
 {
   dat <- open_dataset(ch3_file)
-  
-  new_sample_name <- paste0(coverage, 
-                            "xSample_", 
-                            gsub(".*?/(.*?)-..ch3", "\\1", ch3_file), 
-                            sample_name_suffix)
   
   read_data <- 
     dat |>
@@ -58,29 +51,28 @@ sample_ch3 <- function(ch3_file,
     distinct() |>
     collect()
   
-  full_coverage <- sum(read_data$read_length) / genome_size
-  
-  message("Original Coverage: ", full_coverage)
-  
+  full_coverage <- sum(read_data$read_length) / 3.2e9
   fraction <- coverage / full_coverage
   
-  if (fraction >= 1) {
-    message("Warning: Original coverage less than desired coverage, copying full file.")
-    dat |>
-      mutate(sample_name = new_sample_name) |>
-      write_dataset(
-        path = out_path, 
-        basename_template = paste0(new_sample_name, "-{i}.ch3"))
-  } else {
-    sampled_ids  <- sample(read_data$read_id, fraction * nrow(read_data), 
-                           replace = FALSE)
-    
-    dat |>
-      filter(
-        read_id %in% sampled_ids) |>
-      mutate(sample_name = new_sample_name) |>
-      write_dataset(
-        path = out_path, 
-        basename_template = paste0(new_sample_name, "-{i}.ch3"))
-  }
+  sampled_ids <- sample(
+    read_data$read_id, 
+    size = min(fraction * nrow(read_data), nrow(read_data)), 
+    replace = FALSE
+  )
+  
+  # Generate a valid new sample name
+  new_sample_name <- paste0(
+    coverage, "xSample_",
+    gsub(".*/(.*)-..ch3", "\\1", ch3_file),  # Avoid using '/' in the name
+    sample_name_suffix
+  )
+  
+  # Write the dataset with a valid basename template
+  dat |>
+    filter(read_id %in% sampled_ids) |>
+    mutate(sample_name = new_sample_name) |>
+    write_dataset(
+      path = out_path, 
+      basename_template = paste0(new_sample_name, "-{i}.ch3")
+    )
 }
