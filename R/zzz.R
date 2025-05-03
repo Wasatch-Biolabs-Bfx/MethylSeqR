a609ed240970b8fc350c54f9e9fd495cb15437df
jonathonthill <jhill@byu.edu>
2025-05-02 20:59:21 -0600

    small edits to logo

--- R/zzz.R
+++ R/zzz.R
@@ -3,12 +3,13 @@
   pv <- packageVersion(pkg)
   msg1 <- 
     glue::glue("========================================",    
-               "  ╔╦╗╔═╗╔╦╗╦ ╦╦ ╦╦  ╔═╗╔═╗╔═╗ ╦═╗",
-               "  ║║║║╣  ║ ╠═╣╚╦╝║  ╚═╗║╣ ║═╬╗╠╦╝",
-               "  ╩ ╩╚═╝ ╩ ╩ ╩ ╩ ╩═╝╚═╝╚═╝╚═╝╚╩╚═{pv}",
-               "========================================")
+               "  ╔╦╗╔═╗╔╦╗╦ ╦╦ ╦╦  ╔═╗╔═╗╔═╗ ╦═╗       ",
+               "  ║║║║╣  ║ ╠═╣╚╦╝║  ╚═╗║╣ ║═╬╗╠╦╝       ",
+               "  ╩ ╩╚═╝ ╩ ╩ ╩ ╩ ╩═╝╚═╝╚═╝╚═╝╚╩╚═{pv}   ",
+               "========================================",
+               .sep = "\n")
   
-  msg2 <- paste("MethylSeqR", packageVersion(pkg))
+  msg2 <- paste("MethylSeqR v", packageVersion(pkg))
   
   if (interactive()) {
     packageStartupMessage(msg1)
@@ -18,7 +19,8 @@
   
   packageStartupMessage("Created by Wasatch Biolabs")
   packageStartupMessage("Research & Clinical Nanopore Sequencing")
-  packageStartupMessage("www.wasatchbiolabs.com\n")
+  packageStartupMessage("www.wasatchbiolabs.com")
+  packageStartupMessage("")
   packageStartupMessage("This package is licensed for personal") 
   packageStartupMessage("and internal research use only.")
   packageStartupMessage("See the LICENSE file or visit")
